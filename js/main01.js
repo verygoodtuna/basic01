46345a55267c58c1cd4bdb0ba297c909be9fc925
verygoodtuna <133613555+verygoodtuna@users.noreply.github.com>
2023-05-17 16:47:49 +0900

    Add files via upload

---
+++ js/main01.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.mainSlide').slick({
+        //arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        pauseOnHover: false,
+    });
+})
